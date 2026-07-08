d61dd09bfa550503c90f61a8e01aa0546953f9c4
sashavershkova <a.m.vershkova@gmail.com>
2026-07-08 16:48:12 -0700

    feat: integrate Cognito login in frontend

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthProvider } from 'react-oidc-context'
 import './index.css'
 import App from './App.tsx'
+import { cognitoAuthConfig } from './services/auth/cognitoConfig.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <AuthProvider {...cognitoAuthConfig}>
+      <App />
+    </AuthProvider>
   </StrictMode>,
 )
