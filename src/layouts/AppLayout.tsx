589bae14ef83d18537b59ec8632dfd754386ccdb
Mariya Mokrynska <mariyamokrynska1994@gmail.com>
2026-07-30 00:16:19 -0700

    Wire Sidebar into AppLayout alongside Header

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,11 +1,18 @@
 import { Outlet } from 'react-router-dom'
 import Header from '../components/navigation/Header/Header'
+import Sidebar from '../components/navigation/Sidebar/Sidebar'
+import './AppLayout.css'
 
 function AppLayout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <div className="app-shell">
+        <Sidebar />
+        <div className="app-shell-content">
+          <Outlet />
+        </div>
+      </div>
     </>
   )
 }
